9c830a9e4123857fda8e85f6ec3877f06941c575
Nate Beauregard <nathanbeauregard@gmail.com>
2024-05-28 08:18:03 -0400

    Small updates to rework dependencies

--- x/xcvm/keeper/msg_server.go
+++ x/xcvm/keeper/msg_server.go
@@ -28,7 +28,7 @@ func (k Keeper) SendTransferIntent(goCtx context.Context, msg *types.MsgSendTran
 		return nil, err
 	}
 
-	err := k.SendValidatedTransferIntent(ctx, msg)
+	err := k.SendEthTransferIntent(ctx, msg)
 	if err != nil {
 		return nil, err
 	}
